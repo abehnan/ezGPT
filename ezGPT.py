15b90570c3bb44bbd6a6c1341a6f9ec0dd1db6e9
abehnan <abehnan@gmail.com>
2023-03-29 16:29:36 -0400

    Added basic logging.

--- ezGPT.py
+++ ezGPT.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from datetime import datetime
 import json
 import os
 import requests
@@ -11,12 +12,39 @@ SYSTEM_MESSAGE = "You are a helpful assistant."
 TEMPERATURE = 1
 
 
+def setup_log_file():
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    if not os.path.exists(script_dir + "/logs"):
+        os.makedirs("logs")
+
+    clean_input = user_input[:50]
+    clean_input = "".join([c for c in clean_input if c.isalpha() or c.isdigit() or c == ' ']).rstrip()
+    clean_input = clean_input.replace(' ', '_')
+
+    file_name = datetime.now().strftime("%Y_%m_%d__%H_%M_%S_") + clean_input + ".md"
+    file = open(script_dir + '/logs/' + file_name, "a")
+    file.write("# " + file_name + "\n")
+    return file
+
+
+def log(message):
+    print(message)
+
+    if log_file:
+        log_file.write(message + '\n')
+
+
+def log_user_input_template():
+    log("\n")
+    log("---")
+    log("## User")
+    print("---")  # looks nicer without bottom separator in markdown viewers
+
+
 def get_user_input():
-    print()
-    print("---")
-    print("## User")
-    print("---")
-    user_input = ""
+    log_user_input_template()
+    result = ""
     empty_count = 0
 
     while True:
@@ -30,9 +58,9 @@ def get_user_input():
         if empty_count >= NUM_EMPTY_LINES_TO_SEND_REQUEST:
             break
 
-        user_input += line + "\n"
+        result += line + "\n"
 
-    return user_input
+    return result
 
 
 def create_post_data():
@@ -57,23 +85,33 @@ def send_request():
 def consume_response(response):
     message = json.loads(response.text)["choices"][0]["message"]
     conversation.append(message)
-    print(message["content"])
+    log(message["content"])
 
 
 def respond():
-    print("---")
-    print("## AI")
-    print("---")
+    log("---")
+    log("## AI")
+    print("---")  # looks nicer without bottom separator in markdown viewers
     response = send_request()
     consume_response(response)
 
 
 if __name__ == "__main__":
     conversation = [{"role": "system", "content": SYSTEM_MESSAGE}]
+    user_input = ''
+    log_file = None
 
     if len(sys.argv) > 1:
-        conversation.append({"role": "user", "content": ' '.join(sys.argv[1:])})
-        respond()
+        user_input = ' '.join(sys.argv[1:])
+        log_file = setup_log_file()
+    else:
+        user_input = get_user_input()
+        log_file = setup_log_file()
+
+    log_user_input_template()
+    log(user_input + "\n")
+    conversation.append({"role": "user", "content": user_input})
+    respond()
 
     while True:
         conversation.append({"role": "user", "content": get_user_input()})
